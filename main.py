efb81060accb43ea04afbc64a7b8a96f5f9b261b
littlefean <2028140990@qq.com>
2023-02-11 22:55:22 +0800

    实现了清空列表

--- main.py
+++ main.py
@@ -4,61 +4,104 @@
 """
 
 import sys
+import os
+import threading
+import time
 
 from PyQt5.QtWidgets import *
+from pynput.keyboard import Listener
+
 from myitem import MyItem
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.dic = {}  # {title: divEle}
+        # 主纵向布局
+        self.layoutMain = QVBoxLayout()
+        # 第一行布局
+        self.layoutTopLine = QHBoxLayout()
+
+        self.layoutList = QVBoxLayout()
+        self.layoutScrollDiv = QVBoxLayout()
+
         self.init()
+        print("init End")
 
     def init(self):
         self.setWindowTitle("sss")
         self.resize(800, 500)
 
         # ====
-        layoutMain = QVBoxLayout()
+        self.layoutMain = QVBoxLayout()
 
         # == 第一行
-        layoutTopLine = QHBoxLayout()
-        btnNew = QPushButton("新建")
+
+        btnNew = QPushButton("打开")
         btnNew.setStyleSheet("background-color: rgba(255, 0, 0, 0.5);color:white")
 
         btnSearch = QPushButton("搜索")
         inputDiv = QLineEdit()
+        inputDiv.setPlaceholderText("请输入内容")
+        inputDiv.textChanged[str].connect(self.inputChangeFunc)
 
-        layoutTopLine.addWidget(btnNew)
-        layoutTopLine.addWidget(inputDiv)
-        layoutTopLine.addWidget(btnSearch)
+        # layoutTopLine.addWidget(btnNew)
+        self.layoutTopLine.addWidget(inputDiv)
+        self.layoutTopLine.addWidget(btnSearch)
 
-        layoutMain.addLayout(layoutTopLine)
+        self.layoutMain.addLayout(self.layoutTopLine)
 
         # 下面的列表
-        layoutList = QVBoxLayout()
 
         scrollDiv = QWidget()
 
-        layoutScrollDiv = QVBoxLayout()
-        scrollDiv.setLayout(layoutScrollDiv)
+        scrollDiv.setLayout(self.layoutScrollDiv)
 
-        for i in range(50):
-            if i == 30:
-                layoutScrollDiv.addLayout(MyItem("aaa" * 10, "bb"))
-            else:
-                layoutScrollDiv.addLayout(MyItem("aaa", "bb"))
+        # 读取追加
+        for fileName in os.listdir("myData"):
+            oneItem = MyItem.fromFileName(fileName)
+            self.layoutScrollDiv.addLayout(oneItem)
+            self.dic[fileName] = oneItem
 
         scroll = QScrollArea()
         scroll.setMinimumHeight(500)
         scroll.setWidget(scrollDiv)
         # ==== 追加纵向布局
 
-        layoutMain.addLayout(layoutList)
-        layoutMain.addWidget(scroll)
+        self.layoutMain.addLayout(self.layoutList)
+        self.layoutMain.addWidget(scroll)
 
-        self.setLayout(layoutMain)
+        self.setLayout(self.layoutMain)
         return
+
+    def inputChangeFunc(self, string):
+        """搜索某个条目"""
+        print(string)
+        print(self.layoutScrollDiv.count())
+
+        for k, v in self.dic.items():
+            v.clear()
+
+        ...
+
+
+def addHook():
+    """绑定热键"""
+    lastKey = None
+
+    def on_press(key):
+        nonlocal lastKey
+        if str(key) == "Key.alt_l" and str(lastKey) == "'1'":
+            print("成了")
+        lastKey = key
+
+    def f():
+        with Listener(on_press=on_press) as listener:
+            listener.join()
+
+    threading.Thread(target=f).start()
 
 
 def main():
--- myitem.py
+++ myitem.py
@@ -1,4 +1,8 @@
+# -*- encoding : utf-8 -*-
 from PyQt5.QtWidgets import *
+
+import win32clipboard
+import win32con
 
 
 class MyItem(QHBoxLayout):
@@ -8,34 +12,74 @@ class MyItem(QHBoxLayout):
         super().__init__()
         self.title = title
         self.content = content
+        # ====
+        # 标题
+        self.labelLeft = QLabel()
+        # 内容
+        self.labelRight = QLabel()
+        # 复制按钮
+        self.btnCopy = QPushButton("复制")
+        # # 删除按钮
+        # self.btnDel = QPushButton("X")
+        # ====
         self.init()
 
+    def copyAction(self):
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(win32con.CF_UNICODETEXT, self.content)
+        win32clipboard.CloseClipboard()
+
     def init(self):
-        # 标题
-        labelLeft = QLabel()
-        labelLeft.setText(self.title)
-        labelLeft.setStyleSheet("background-color: skyblue")
-        # 内容
-        labelRight = QLabel()
-        labelRight.setText(self.content)
-        labelRight.setStyleSheet("outline: solid 1px")
 
-        # 复制按钮
-        btnCopy = QPushButton()
-        btnCopy.setText("复制")
-        btnCopy.setMaximumWidth(40)
+        self.labelLeft.setText(self.title)
+        self.labelLeft.setStyleSheet("background-color: skyblue")
 
-        # 删除按钮
-        btnDel = QPushButton()
-        btnDel.setText("X")
-        btnDel.setStyleSheet("background-color: pink;")
-        btnDel.setMaximumWidth(20)
+        self.labelRight.setText(self.showContent())
+        self.labelRight.setStyleSheet("outline: solid 1px")
+
+        self.btnCopy.clicked.connect(self.copyAction)
+        self.btnCopy.setMaximumWidth(40)
+
+        # self.btnDel.setStyleSheet("background-color: pink;")
+        # self.btnDel.setMaximumWidth(20)
 
         # --- 添加
-        self.addWidget(labelLeft)
-        self.addWidget(labelRight)
-        self.addWidget(btnCopy)
-        self.addWidget(btnDel)
+        self.addWidget(self.labelLeft)
+        self.addWidget(self.labelRight)
+        self.addWidget(self.btnCopy)
+        # self.addWidget(self.btnDel)
+        ...
+
+    def showContent(self):
+        """返回显示在界面上的简短缩略的内容"""
+        res = self.content[:30]
+        res = res.replace("\n", " ")
+        return res
+
+    @classmethod
+    def fromFileName(cls, fileName: str):
+        """通过文件名读取"""
+        try:
+            with open(f"myData/{fileName}", encoding="utf-8") as f:
+                content = f.read()
+            # if len(content) > 15:
+            #     content = content[:15] + "..."
+            return cls(fileName, content)
+        except UnicodeDecodeError:
+            print(fileName, "无法读取内容或者文件名")
+
+    def clear(self):
+        """清空当前这个条目里的所有内容"""
+        print("触发了clear")
+        self.removeWidget(self.btnCopy)
+        self.btnCopy.setParent(None)
+
+        # self.removeWidget(self.btnDel)
+        self.removeWidget(self.labelLeft)
+        self.labelLeft.setParent(None)
+        self.removeWidget(self.labelRight)
+        self.labelRight.setParent(None)
         ...
 
     ...
